958df9e80f2aaac996ec81112eb7b4d046179439
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-12-07 12:10:32 +0000

    add association

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,4 @@
 class Favorite < ApplicationRecord
+  belongs_to :post
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :image
+  has_many :posts, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
