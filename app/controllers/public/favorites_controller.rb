9a63dd912390fbd27d0454658d8ec76a6a9ea312
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-12-18 04:38:17 +0000

    いいねの非同期化

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -7,16 +7,14 @@ class Public::FavoritesController < ApplicationController
   end
 
   def create
-    item = Item.find(params[:item_id])
-    favorite = current_user.favorites.new(item_id: item.id)
+    @item = Item.find(params[:item_id])
+    favorite = current_user.favorites.new(item_id: @item.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
-    item = Item.find(params[:item_id])
-    favorite = current_user.favorites.find_by(item_id: item.id)
+    @item = Item.find(params[:item_id])
+    favorite = current_user.favorites.find_by(item_id: @item.id)
     favorite.destroy
-    redirect_to request.referer
   end
 end
