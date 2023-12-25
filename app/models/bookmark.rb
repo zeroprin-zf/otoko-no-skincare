5595b81519d697a9d45cb683b3ec3c12100f3c15
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-12-25 01:50:36 +0000

    Fix conflicting changes in raty

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,16 @@
 class Bookmark < ApplicationRecord
+  #has_one_attached :image
   belongs_to :user
   belongs_to :item
 
   validates_uniqueness_of :item_id, scope: :user_id
   validates :user_id, uniqueness: { scope: :item_id}
+
+  #def get_image
+   # unless image.attached?
+    #  file_path = Rails.root.join('app/assets/images/no_image_square.jpg')
+     # image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    #end
+    #image
+  #end
 end
